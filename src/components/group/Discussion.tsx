cd2071edc5df6ae89af7c5d7b190d21a56cb7f0a
adedotxn <adedotxn.dev@gmail.com>
2023-06-02 11:57:37 +0100

    fix: missing props causing build failure

--- src/components/group/Discussion.tsx
+++ src/components/group/Discussion.tsx
@@ -1,4 +1,4 @@
-import { Container, Flex, Heading, Spacer, Text } from '@chakra-ui/react';
+import { Container, Flex, Grid, Heading, Spacer, Text } from '@chakra-ui/react';
 import DiscussionCards from './DiscussionCards';
 
 const Discussion = () => {
@@ -12,7 +12,19 @@ const Discussion = () => {
         <Text fontWeight="medium">See All</Text>
       </Flex>
 
-      <DiscussionCards />
+      <Grid mt={12} gap="2rem" pb={20}>
+        {[
+          { name: 'Philip Adewole', username: 'Philip The Great' },
+          { name: 'Sarah Wong', username: 'Sarah The Magneficent' },
+          { name: 'Assad', username: 'nottherealalanturing' },
+        ].map((post, index) => (
+          <DiscussionCards
+            key={index}
+            name={post.name}
+            username={post.username}
+          />
+        ))}
+      </Grid>
     </Container>
   );
 };
