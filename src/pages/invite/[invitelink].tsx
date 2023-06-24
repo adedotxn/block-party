c7c6da2381909c6bd94694f443ec6debe8940c3b
adedotxn <adedotxn.dev@gmail.com>
2023-06-24 22:24:02 +0100

    fix: invalid link flash

--- src/pages/invite/[invitelink].tsx
+++ src/pages/invite/[invitelink].tsx
@@ -8,6 +8,7 @@ import { NextPage } from 'next';
 
 const InvitePage: NextPage = () => {
   const router = useRouter();
+
   const { invitelink } = router.query;
 
   const isValidInvite = validateInviteLink(invitelink as string);
@@ -27,41 +28,43 @@ const InvitePage: NextPage = () => {
         <Box>
           <Flex direction="column" align="center" gap={10}>
             <Image src={logo} width={130} height={130} alt="logo" />
-            <Flex
-              direction="column"
-              align="center"
-              justify="center"
-              my="30"
-              w="100%" // Set the width to 100% of the container
-              maxWidth="247px" // Set the maximum width
-              borderRadius="9px"
-              padding="10px"
-              gap={'10px'}
-              background="#FFFFFF"
-            >
-              <Text
-                fontWeight={700}
-                fontSize="lg"
-                lineHeight="15px"
-                letterSpacing="-0.02em"
-                color="#CC2900"
+            {invitelink !== undefined ? (
+              <Flex
+                direction="column"
+                align="center"
+                justify="center"
+                my="30"
+                w="100%" // Set the width to 100% of the container
+                maxWidth="247px" // Set the maximum width
+                borderRadius="9px"
+                padding="10px"
+                gap={'10px'}
+                background="#FFFFFF"
               >
-                Invalid Invite Link
-              </Text>
-              <Text
-                fontWeight={500}
-                fontSize="xl"
-                lineHeight="22px"
-                letterSpacing="-0.03em"
-                color="#3D3D3E"
-                textAlign="center"
-              >
-                The invite link you used is either invalid or has been revoked.
-                Please double-check the link and try again. If you believe this
-                is an error, please contact your board facilitator for
-                assistance.
-              </Text>
-            </Flex>
+                <Text
+                  fontWeight={700}
+                  fontSize="lg"
+                  lineHeight="15px"
+                  letterSpacing="-0.02em"
+                  color="#CC2900"
+                >
+                  Invalid Invite Link
+                </Text>
+                <Text
+                  fontWeight={500}
+                  fontSize="xl"
+                  lineHeight="22px"
+                  letterSpacing="-0.03em"
+                  color="#3D3D3E"
+                  textAlign="center"
+                >
+                  The invite link you used is either invalid or has been
+                  revoked. Please double-check the link and try again. If you
+                  believe this is an error, please contact your board
+                  facilitator for assistance.
+                </Text>
+              </Flex>
+            ) : null}
           </Flex>
         </Box>
       </Flex>
